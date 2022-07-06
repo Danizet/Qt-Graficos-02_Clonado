fd88522e8e4a46b2ecd2db37922feeeb0ab77a91
Danizet <lproanoc1@est.ups.edu.ec>
2022-07-06 13:00:54 -0500

    Poniendo ejees y promedio

--- principal.cpp
+++ principal.cpp
@@ -35,7 +35,7 @@ void Principal::dibujar()
 
     // Crear un pincel para los bordes
     QPen pincel;
-    pincel.setWidth(5);
+    pincel.setWidth(1);
     pincel.setColor(Qt::black);
     pincel.setJoinStyle(Qt::MiterJoin);
 
@@ -62,7 +62,7 @@ void Principal::dibujar()
     int incYN3 = this->incY(altoN3);
 
     // Dibujar primera barra
-    painter.drawRect(x+50, y+50+incYN1,100,altoN1-1);
+    painter.drawRect(80, y+50+incYN1,100,altoN1-1);
 
     // Crear un objeto color para el relleno
     QColor colorRelleno1(100,200,100);
@@ -79,7 +79,7 @@ void Principal::dibujar()
     painter.setBrush(colorRelleno1);
 
     // Dibujar segunda barra
-    painter.drawRect(x+170, y+50+incYN2, 100, altoN2-1);
+    painter.drawRect(200, y+50+incYN2, 100, altoN2-1);
 
     // Creando los colores de la tercera barra
     QColor cRellenoBarra3(255, 100, 100);
@@ -91,9 +91,66 @@ void Principal::dibujar()
     painter.setBrush(cRellenoBarra3);
 
     // Dibujar tercera barra
-    painter.drawRect(x+290,y+50+incYN3,100,altoN3-1);
-}
+    painter.drawRect(320,y+50+incYN3,100,altoN3-1);
 
+    //titulo
+    painter.setPen(Qt::red);
+    painter.setFont(QFont("Arial",12));
+    painter.drawText(220,25,"Grafica de promedios");
+
+    // Promedio
+    QColor line(106,38,111);
+    pincel.setColor(line);
+    pincel.setWidth(3);
+    painter.setPen(pincel);
+
+    float promedio = (nota1 + nota2 + nota3) / 3;
+    int prom = this->getAlto(promedio);
+
+    painter.drawLine(x+50,y+450-prom,443,y+450-prom); //barra prom
+    // Ejes
+    QColor eje(0,0,0);
+    pincel.setColor(eje);
+    pincel.setWidth(3);
+    painter.setPen(pincel);
+    painter.drawLine(50,480,50,30); //y
+    painter.drawLine(10,450,455,450); //x
+
+    // x y y
+
+    painter.setPen(Qt::black);
+    painter.setFont(QFont("Arial",15));
+    painter.drawText(460,455,"x");
+    painter.drawText(45,25,"y");
+
+    // Divisiones barras
+
+    painter.drawText(120,472,"N1");
+    painter.drawText(240,472,"N2");
+    painter.drawText(360,472,"N3");
+
+    // Divisiones numeros
+
+    painter.setFont(QFont("Calibri",25));
+
+    // ciclo
+    for(int i=0;i<=20;i+=5)
+    {
+        painter.drawText(28,392,"i");
+    }
+   // return painter;
+    /*
+    painter.drawText(28,432,"5 -------------------");
+    painter.drawText(28,412,"10 ----------------");
+    painter.drawText(28,392,"15 ----------------");
+    painter.drawText(28,372,"20 ----------------");
+    painter.drawText(28,352,"25 ----------------");
+    painter.drawText(28,252,"50 ----------------");
+    painter.drawText(28,152,"75 ----------------");
+    painter.drawText(28,57,"99 ----------------");
+    */
+
+}
 int Principal::getAlto(int valor)
 {
     return 4 * valor;
